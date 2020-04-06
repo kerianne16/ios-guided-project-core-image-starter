d179061fc919885b5afb1f4f2d307e1e9ac17230
Keri Levesque <kerianne166@gmail.com>
2020-04-06 13:13:51 -0400

    filtering test image and updating display

--- PhotoFilter/PhotoFilterViewController.swift
+++ PhotoFilter/PhotoFilterViewController.swift
@@ -1,9 +1,17 @@
 import UIKit
 import CoreImage
+import CoreImage.CIFilterBuiltins
 import Photos
 
 class PhotoFilterViewController: UIViewController {
 
+    private var originalImage: UIImage? {
+        didSet {
+            updateViews()
+        }
+    }
+   private var context = CIContext(options: nil)
+    
 	@IBOutlet weak var brightnessSlider: UISlider!
 	@IBOutlet weak var contrastSlider: UISlider!
 	@IBOutlet weak var saturationSlider: UISlider!
@@ -11,8 +19,42 @@ class PhotoFilterViewController: UIViewController {
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-
+        
+        let filter = CIFilter.colorControls()
+        print(filter)
+        
+        print(filter.attributes)
+        
+        //test the filter quickly
+        originalImage = imageView.image
 	}
+    func updateViews(){
+        if let originalImage = originalImage {
+            imageView.image = filterImage(originalImage)
+        } else {
+            imageView.image = nil // placeholder image
+        }
+    }
+  
+    func filterImage(_ image: UIImage) -> UIImage? {
+        // UIImage -> CGImage (Core Graphics) -> CIImage (Core Image)
+        guard let cgImage = image.cgImage else { return nil }
+        let ciImage = CIImage(cgImage: cgImage)
+        // Filter = recipe
+        let filter = CIFilter.colorControls()
+        filter.inputImage = ciImage
+        filter.brightness = brightnessSlider.value
+        filter.contrast = contrastSlider.value
+        filter.saturation = saturationSlider.value
+        guard let outputCIImage = filter.outputImage else { return nil }
+        // Render the image
+        guard let outputCGImage = context.createCGImage(outputCIImage,
+                                                        from: CGRect(origin: .zero, size: image.size)) else {
+                                                            return nil
+        }
+        // CIImage -> CGImage -> UIImage
+        return UIImage(cgImage: outputCGImage)
+    }
 	
 	// MARK: Actions
 	
@@ -29,14 +71,17 @@ class PhotoFilterViewController: UIViewController {
 	// MARK: Slider events
 	
 	@IBAction func brightnessChanged(_ sender: UISlider) {
+        updateViews()
 
 	}
 	
 	@IBAction func contrastChanged(_ sender: Any) {
+        updateViews()
 
 	}
 	
 	@IBAction func saturationChanged(_ sender: Any) {
+        updateViews()
 
 	}
 }
